0a2048bf4dda6c99e7117a059b3522f2cc66f19b
MamcoSy <mamco.aly.sy@gmail.com>
2021-01-19 15:28:41 +0000

    Update view

--- src/View/View.php
+++ src/View/View.php
@@ -8,55 +8,55 @@ use LiteFramework\FileSystem\FileSystem;
 
 class View
 {
-	/**
-	 * View Constructor
-	 */
-	private function __construct() {}
+    /**
+     * View Constructor
+     */
+    private function __construct() {}
 
-	/**
-	 * Render a view
-	 *
-	 * @param  string   $view
-	 * @param  array    $data
-	 * @return string
-	 */
-	public static function render(string $view, array $data = []): string
-	{
-		$errors =  Session::flash('errors');
-		$old    =  Session::flash('old');
+    /**
+     * Render a view
+     *
+     * @param  string   $view
+     * @param  array    $data
+     * @return string
+     */
+    public static function render(string $view, array $data = []): string
+    {
+        $data['errors'] = Session::flash('errors');
+        $data['old']    = Session::flash('old');
 
-		return static::bladeRenderer(str_replace(['\\', '/', '.'], DS, $view), $data);
-	}
+        return static::bladeRenderer(str_replace(['\\', '/', '.'], DS, $view), $data);
+    }
 
-	/**
-	 * Render a view with simple php
-	 *
-	 * @param  string   $view
-	 * @param  array    $data
-	 * @return string
-	 */
-	public static function Simplerenderer(string $view, array $data = []): string
-	{
-		ob_start();
-		extract($data);
-		FileSystem::requireFile('views' . DS . $view . '.php');
-		$content = ob_get_clean();
-		ob_clean();
+    /**
+     * Render a view with simple php
+     *
+     * @param  string   $view
+     * @param  array    $data
+     * @return string
+     */
+    public static function Simplerenderer(string $view, array $data = []): string
+    {
+        ob_start();
+        extract($data);
+        FileSystem::requireFile('views' . DS . $view . '.php');
+        $content = ob_get_clean();
+        ob_clean();
 
-		return $content;
-	}
+        return $content;
+    }
 
-	/**
-	 * Render a view with blate templating engine
-	 *
-	 * @param  string   $view
-	 * @param  array    $data
-	 * @return string
-	 */
-	public static function bladeRenderer(string $view, array $data = []): string
-	{
-		$blade = new Blade(FileSystem::getPath('views'), FileSystem::getPath('storage/cache'));
+    /**
+     * Render a view with blate templating engine
+     *
+     * @param  string   $view
+     * @param  array    $data
+     * @return string
+     */
+    public static function bladeRenderer(string $view, array $data = []): string
+    {
+        $blade = new Blade(FileSystem::getPath('views'), FileSystem::getPath('storage/cache'));
 
-		return $blade->make($view, $data)->render();
-	}
+        return $blade->make($view, $data)->render();
+    }
 }
